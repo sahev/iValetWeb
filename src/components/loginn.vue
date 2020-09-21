d8e8b48c83680ce52c1511f68137292017b2fb36
sahev <s4mus.sh@gmail.com>
2020-09-21 20:15:38 -0300

    Inclusão de página de login

---
+++ src/components/loginn.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-form  @submit.stop.prevent>
+      <label for="feedback-user">User ID</label>
+      <b-input v-model="userId" :state="validation" id="feedback-user"></b-input>
+      <b-form-invalid-feedback :state="validation">
+        Your user ID must be 5-12 characters long.
+      </b-form-invalid-feedback>
+      <b-form-valid-feedback :state="validation">
+        Looks Good.
+      </b-form-valid-feedback>
+     </b-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        userId: ''
+      }
+    },
+    computed: {
+      validation() {
+        return this.userId.length > 4 && this.userId.length < 13
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-app id="inspire">
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col md="4">
+          <v-card class="elevation-8">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>iValet</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form v-model="valid" ref="form" lazy-validation>
+                <v-text-field
+                  v-model="username"
+                  :rules="userRules"
+                  label="Usuário"
+                  type="text"
+                  required
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="password"
+                  :rules="userRules"
+                  label="Senha"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+
+            <v-btn :disabled="!valid" color="primary" @click="login">Login</v-btn>
+
+            <v-card-text>
+              <a href="#/forgot">Esqueci a senha</a>
+            </v-card-text>
+            <v-card-text>
+              Não possui login?
+              <a href="#/signup">Cadastre-se</a>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      valid: true,
+      username: "",
+      password: "",
+      userRules: [(v) => !!v || "Digite um usuário"],
+      passRules: [(v) => !!v || "Digite uma senha"],
+    };
+  },
+  methods: {
+    login() {
+      this.$refs.form.validate();
+      console.log(this.username, this.password);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
