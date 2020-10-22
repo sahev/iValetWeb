588e321afd5d841881727ddf5b3dfd8f21d4c047
sahev <s4mus.sh@gmail.com>
2020-10-22 14:23:55 -0300

    Alteração de atividades recentes

--- src/components/Recents/RecentsActivity.vue
+++ src/components/Recents/RecentsActivity.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-navigation-drawer app clipped right>
-
-    <v-list three-line>
-
-      <template v-for="(item, index) in recents">
-        <v-subheader v-if="item.header" :key="item.header"
+  <v-navigation-drawer app clipped right >
+    <v-list two-line>
+              <v-subheader
           >Atividades recentes</v-subheader
         >
+      <template v-for="(item, index) in reverse">
+        <v-subheader v-if="reverse.header" :key="index"
+          ></v-subheader>
 
         <v-divider
           v-else-if="item.divider"
@@ -14,22 +14,22 @@
           :inset="item.inset"
         ></v-divider>
 
-        <v-list-item v-else :key="item.title">
+        <v-list-item v-else :key="item.index">
           <v-list-item-avatar>
-            <v-img :src="item.avatar"></v-img>
+            <!-- <v-img :src="item.avatar"></v-img> -->
+            <v-avatar color="secondary" size="43">
+              <span class="white--text"> img </span>
+            </v-avatar>
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title v-html="item.title"></v-list-item-title>
-            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+            <v-list-item-title v-html="item.placa"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.status[0]"></v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
-
       </template>
-      <v-btn @click="log()">{{ itemss }}</v-btn>
     </v-list>
   </v-navigation-drawer>
-
 </template>
 
 <script>
@@ -37,71 +37,30 @@ import { mapState, mapMutations, mapActions } from 'vuex';
 
 export default {
   name: 'RecentsActivity',
-  props: ['itemss'],
   methods: {
     ...mapActions(['getOpeneds']),
     ...mapMutations(['getVehicles']),
-    log() {
-      console.log('localData: ', this.list);
-    },
   },
   computed: {
     ...mapState({
       openedTransactions: (a) => a.socket.openedTransactions,
       finishedTransactions: (a) => a.socket.finishedTransactions,
+      recentss: (a) => a.socket.recents,
     }),
-  },
-  watch: {
-    openedTransactions(newValue, oldValue) {
-      console.log('watch opened', newValue, oldValue);
+    reverse() {
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      return this.recentss.reverse();
     },
-    // finishedTransactions(newValue) {
-    //   const a = newValue;
-    //   console.log('watch finished');
-    //   this.list.push(a.slice(-1)[0]);
-    // },
-  },
-  created() {
-    this.getVehicles();
   },
   data() {
     return {
-      list: [],
-      recents: [
-        { header: 'Atividades Recentes' },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'ERO1232',
-          subtitle: 'Entrada',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'ERO1234',
-          subtitle: 'Saída',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'ERO1235',
-          subtitle: 'Saída',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-          title: 'ERO1236',
-          subtitle: 'Entrada',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'ERO1237',
-          subtitle: 'Entrada',
-        },
-      ],
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+.v-list{
+  height: auto;
+}
+</style>
