066584cce6c2ceb12248bbaf54d73d4f797d9455
sahev <s4mus.sh@gmail.com>
2020-11-11 18:21:18 -0300

    Ajuste do método de inserção

--- src/components/TableList/components/AddRotativePrice.vue
+++ src/components/TableList/components/AddRotativePrice.vue
@@ -2,68 +2,68 @@
   <v-container>
     <div v-for="(item, i) in items" :key="i">
       <v-form ref="form" lazy-validation>
-      <v-row>
-        <v-col id="col" cols="2" sm="2">
-          <v-text-field
-            v-model="item.from"
-            label="De:"
-            prepend-icon="mdi-clock-time-four-outline"
-            :rules="[
-              (val) => (val || '').length > 0 || 'Insira um tempo mínimo',
-            ]"
-            color="purple darken-2"
-            required
-          ></v-text-field>
-        </v-col>
+        <v-row>
+          <v-col id="col" cols="2" sm="2">
+            <v-text-field
+              v-model="item.from"
+              label="De:"
+              prepend-icon="mdi-clock-time-four-outline"
+              :rules="[
+                (val) => (val || '').length > 0 || 'Insira um tempo mínimo',
+              ]"
+              color="purple darken-2"
+              required
+            ></v-text-field>
+          </v-col>
 
-        <v-col id="col" cols="2" sm="2">
-          <v-text-field
-            v-model="item.to"
-            label="Até:"
-            prepend-icon="mdi-clock-time-four-outline"
-            :rules="[
-              (val) => (val || '').length > 0 || 'Insira um tempo máximo',
-            ]"
-            color="purple darken-2"
-            required
-          ></v-text-field>
-        </v-col>
+          <v-col id="col" cols="2" sm="2">
+            <v-text-field
+              v-model="item.to"
+              label="Até:"
+              prepend-icon="mdi-clock-time-four-outline"
+              :rules="[
+                (val) => (val || '').length > 0 || 'Insira um tempo máximo',
+              ]"
+              color="purple darken-2"
+              required
+            ></v-text-field>
+          </v-col>
 
-        <v-col id="col" cols="2" sm="3">
-          <v-text-field
-            v-model="item.gracePeriod"
-            label="Tolerância:"
-            prepend-icon="mdi-clock-time-four-outline"
-            :rules="[
-              (val) => (val || '').length > 0 || 'Insira a tolerância',
-            ]"
-            color="purple darken-2"
-            required
-          ></v-text-field>
-        </v-col>
+          <v-col id="col" cols="2" sm="3">
+            <v-text-field
+              v-model="item.gracePeriod"
+              label="Tolerância:"
+              prepend-icon="mdi-clock-time-four-outline"
+              :rules="[
+                (val) => (val || '').length > 0 || 'Insira a tolerância',
+              ]"
+              color="purple darken-2"
+              required
+            ></v-text-field>
+          </v-col>
 
-        <v-col id="col" cols="2" sm="2">
-          <v-text-field
-            v-model="item.price"
-            label="Valor:"
-            prepend-icon="mdi-currency-usd"
-            :rules="[
-              (val) => (val || '').length > 0 || 'Insira um valor',
-            ]"
-            color="purple darken-2"
-            required
-          ></v-text-field>
-        </v-col>
-<!-- <div>
+          <v-col id="col" cols="2" sm="2">
+            <v-text-field
+              v-model="item.price"
+              label="Valor:"
+              prepend-icon="mdi-currency-usd"
+              :rules="[(val) => (val || '').length > 0 || 'Insira um valor']"
+              color="purple darken-2"
+              required
+            ></v-text-field>
+          </v-col>
+          <!-- <div>
         <v-icon style="margin-top: 35px;" @click="add(item, i)">mdi-check</v-icon>
 </div> -->
-<div>
-        <v-icon style="margin-top: 35px;" v-if="i > 0" @click="remove(i)">mdi-close</v-icon>
-        </div>
-      </v-row>
+          <div>
+            <v-icon style="margin-top: 35px" v-if="i > 0" @click="remove(i)"
+              >mdi-close</v-icon
+            >
+          </div>
+        </v-row>
       </v-form>
 
-<v-tooltip bottom>
+      <v-tooltip bottom>
         <template v-slot:activator="{ on, attrs }">
           <v-btn
             fab
@@ -81,11 +81,10 @@
         </template>
         <span>Salvar</span>
       </v-tooltip>
+    <div>
+      <v-icon @click="addrows(item)">mdi-plus</v-icon>
     </div>
-          <div>
-        <v-icon @click="addrows()">mdi-plus</v-icon>
-</div>
-
+    </div>
   </v-container>
 </template>
 
@@ -119,8 +118,7 @@ export default {
       data: [],
     };
   },
-  created() {
-  },
+  created() {},
   computed: {
     ...mapState({
       statusButton: (a) => a.addrotative.statusButton,
@@ -162,17 +160,17 @@ export default {
           type: this.type,
           to: Number(data.to),
           from: Number(data.from),
-          gracePeriod: Number(data.gracePeriod),
-          price: Number(data.price),
-          weekDay: this.convertDays(this.id_dw).join(', ').replaceAll(', ', '|'),
+          weekDay: this.convertDays(this.id_dw)
+            .join(', ')
+            .replaceAll(', ', '|'),
           companyId: Number(localStorage.getItem('company')),
+          price: Number(data.price),
           maxPriceValue: 10,
           uniqueIdPrice: this.uniqueId,
+          gracePeriod: Number(data.gracePeriod),
         });
+        this.addPrices(this.data);
 
-        axios.post(`/price/${this.stateInfo.companyId}`, this.data, {
-          headers: { Authorization: `Bearer ${this.stateInfo.token}` },
-        });
         // eslint-disable-next-line no-unused-expressions
         this.setButton(false);
         // eslint-disable-next-line no-unused-expressions
@@ -203,7 +201,7 @@ export default {
       this.data.splice(index, 1);
       this.data.splice(index, 1);
     },
-    addrows() {
+    addrows(data) {
       this.items.push({
         to: null,
         from: null,
@@ -214,13 +212,45 @@ export default {
         maxPriceValue: null,
         uniqueIdPrice: this.uniqueIdPrice,
       });
+      this.data.push({
+        type: this.type,
+        to: Number(data.to),
+        from: Number(data.from),
+        weekDay: this.convertDays(this.id_dw)
+          .join(', ')
+          .replaceAll(', ', '|'),
+        companyId: Number(localStorage.getItem('company')),
+        price: Number(data.price),
+        maxPriceValue: 10,
+        uniqueIdPrice: this.uniqueId,
+        gracePeriod: Number(data.gracePeriod),
+      });
     },
     convertDays(data) {
-      const days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
+      const days = [
+        'MONDAY',
+        'TUESDAY',
+        'WEDNESDAY',
+        'THURSDAY',
+        'FRIDAY',
+        'SATURDAY',
+        'SUNDAY',
+      ];
       return data.map((res) => days[res] || '');
     },
     setUniqueId() {
       return format(new Date(), 'HHmmssSSS');
+    },
+    addPrices(data) {
+      const item = data;
+      item.map((res) => axios.post(
+        '/price',
+        res,
+        {
+          headers: { Authorization: `Bearer ${this.stateInfo.token}` },
+        },
+      ));
+      // item.map((res) => console.log('ressss ', res));
     },
   },
 };
