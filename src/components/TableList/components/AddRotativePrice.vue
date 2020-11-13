2b6e7951b6ecf5cb5bd5cb9b2f5dc0e870c21b18
sahev <s4mus.sh@gmail.com>
2020-11-12 22:50:06 -0300

    Validação adição de campos

--- src/components/TableList/components/AddRotativePrice.vue
+++ src/components/TableList/components/AddRotativePrice.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container>
     <div v-for="(item, i) in items" :key="i">
-      <v-form ref="form" lazy-validation>
+      <v-form ref="form"
+      lazy-validation>
         <v-row>
           <v-col id="col" cols="2" sm="2">
             <v-text-field
@@ -56,6 +57,9 @@
         <v-icon style="margin-top: 35px;" @click="add(item, i)">mdi-check</v-icon>
 </div> -->
           <div>
+
+      <v-icon style="margin-top: 35px" @click="addrows(item, i)">mdi-check</v-icon>
+
             <v-icon style="margin-top: 35px" v-if="i > 0" @click="remove(i)"
               >mdi-close</v-icon
             >
@@ -81,9 +85,6 @@
         </template>
         <span>Salvar</span>
       </v-tooltip>
-    <div>
-      <v-icon @click="addrows(item)">mdi-plus</v-icon>
-    </div>
     </div>
   </v-container>
 </template>
@@ -101,6 +102,7 @@ export default {
   components: {},
   data() {
     return {
+      disableditem: [{}],
       form: {},
       type: 2,
       items: [
@@ -169,61 +171,56 @@ export default {
           uniqueIdPrice: this.uniqueId,
           gracePeriod: Number(data.gracePeriod),
         });
+
+        // console.log(this.$refs.form[index].enabled);
         this.addPrices(this.data);
 
         // eslint-disable-next-line no-unused-expressions
         this.setButton(false);
-        // eslint-disable-next-line no-unused-expressions
-        // this.setPrice(this.data);
+
         console.log('add>', this.data);
-      }
-    },
-    save(data, index) {
-      if (this.$refs.form[index].validate()) {
-        this.data.push({
-          to: data.to,
-          from: data.from,
-          gracePeriod: data.gracePeriod,
-          price: parseInt(data.price, 2),
-          weekDay: this.convertDays(this.id_dw).join(', '),
-          companyId: localStorage.getItem('company'),
-          maxPriceValue: 10,
-          uniqueIdPrice: this.uniqueId,
-        });
-        // eslint-disable-next-line no-unused-expressions
-        this.setButton(false);
-        // eslint-disable-next-line no-unused-expressions
-        // this.setPrice(this.data);
-        // console.log('save item >>>> ', this.data);
       }
     },
     remove(index) {
       this.data.splice(index, 1);
-      this.data.splice(index, 1);
+      this.items.splice(index, 1);
     },
-    addrows(data) {
-      this.items.push({
-        to: null,
-        from: null,
-        gracePeriod: null,
-        price: null,
-        weekDay: null,
-        companyId: null,
-        maxPriceValue: null,
-        uniqueIdPrice: this.uniqueIdPrice,
-      });
-      this.data.push({
-        type: this.type,
-        to: Number(data.to),
-        from: Number(data.from),
-        weekDay: this.convertDays(this.id_dw)
-          .join(', ')
-          .replaceAll(', ', '|'),
-        companyId: Number(localStorage.getItem('company')),
-        price: Number(data.price),
-        maxPriceValue: 10,
-        uniqueIdPrice: this.uniqueId,
-        gracePeriod: Number(data.gracePeriod),
+    addrows(data, i) {
+      if (this.$refs.form[i].validate()) {
+        this.items.push({
+          to: null,
+          from: null,
+          gracePeriod: null,
+          price: null,
+          weekDay: null,
+          companyId: null,
+          maxPriceValue: null,
+          uniqueIdPrice: this.uniqueIdPrice,
+        });
+        this.data.push({
+          type: this.type,
+          to: Number(data.to),
+          from: Number(data.from),
+          weekDay: this.convertDays(this.id_dw)
+            .join(', ')
+            .replaceAll(', ', '|'),
+          companyId: Number(localStorage.getItem('company')),
+          price: Number(data.price),
+          maxPriceValue: 10,
+          uniqueIdPrice: this.uniqueId,
+          gracePeriod: Number(data.gracePeriod),
+        });
+        // console.log(this.$refs.form[i].disabled);
+        // this.$refs.form[i].disabled = true;
+        this.disableForm(i);
+        // document.getElementById('myButton').disabled = true;
+        // console.log(document.getElementById('myButton'));
+      }
+    },
+    disableForm(i) {
+      const vm = this;
+      vm.$nextTick(() => {
+        vm.$refs.form[i].disabled = true;
       });
     },
     convertDays(data) {
